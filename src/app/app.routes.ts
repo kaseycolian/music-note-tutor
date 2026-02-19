30253eacbef21856685619115a30a5d163489ad0
Kasey Colian <kasey.colian@leadingedje.com>
2026-02-19 00:39:38 -0500

    routing fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/tutor',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tutor',
-    loadComponent: () => import('./features/note-tutor/note-tutor/note-tutor').then(m => m.NoteTutor)
+    loadComponent: () =>
+      import('./features/note-tutor/note-tutor/note-tutor').then((m) => m.NoteTutor),
   },
   {
     path: '**',
-    redirectTo: '/tutor'
-  }
+    redirectTo: '',
+  },
 ];
